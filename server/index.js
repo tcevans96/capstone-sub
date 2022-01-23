5c5779663c6cbee5fc7b13dd9001b5cbd04c9c42
tcevans96 <tristanevans57@gmail.com>
2022-01-23 12:46:18 -0600

    begin creating database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use('/static',express.static('public'));
 
+app.use(cors());
+app.use(express.json());
 
 
 app.listen(PORT, ()=>{
