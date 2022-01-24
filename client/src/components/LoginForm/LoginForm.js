dd049078a8cad00d502ecbb585114e75fd3cdb7d
tcevans96 <tristanevans57@gmail.com>
2022-01-24 15:22:27 -0600

    styled signup page

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -15,7 +15,7 @@ export default class LoginForm extends Component {
                     <input className='form__field' name='username' id='username' type="text" />
                     <label htmlFor='password'>Password</label>
                     <input className='form__field' name='password' id='password' type="password" />
-                    <button className='form__button'>Log in</button>
+                    <button className='form__button'>Log In</button>
                 </form>
                 <p>
                     Need an account? <Link className='login__redirect' to="/signup">Sign up</Link>
--- client/src/components/SignupForm/SignupForm.js
+++ client/src/components/SignupForm/SignupForm.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import './SignupForm.scss'
 
 export default class SignupForm extends Component {
-  render() {
-    return <div>Hello World</div>;
-  }
+    
+    state = {
+
+    }
+
+    render() {
+        return (
+            <main className='signup'>
+                <form className='form'>
+                    <label htmlFor='firstName'>First Name</label>
+                    <input className='form__field' name='firstName' id='firstName' type="text" />
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input className='form__field' name='lastName' id='lastName' type="text" />
+                    <label htmlFor='username'>Username</label>
+                    <input className='form__field' name='username' id='username' type="text" />
+                    <label htmlFor='password'>Password</label>
+                    <input className='form__field' name='password' id='password' type="password" />
+                    <label htmlFor='confirmPassword'>Confirm Password</label>
+                    <input className='form__field' name='confirmPassword' id='confirmPassword' type="password" />
+                    <button className='form__button'>Sign Up</button>
+                </form>
+                <p>
+                    Already have an account? <Link className='signup__redirect' to="/login">Log in</Link>
+                </p>
+            </main>
+        );
+    }
 }
